e382caad0f3be1f377d2f907a5120784b28bad5d
Sergey Ponomarev <stokito@gmail.com>
2019-11-09 17:47:04 +0200

    test for inputFileNextLine

--- src/test/java/com/github/stokito/textfile/TextFileStreamReaderTest.java
+++ src/test/java/com/github/stokito/textfile/TextFileStreamReaderTest.java
@@ -2,37 +2,39 @@ package com.github.stokito.textfile;
 
 import org.junit.jupiter.api.Test;
 
+import static com.github.stokito.textfile.TextFileStreamReader.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TextFileStreamReaderTest {
 
     @Test
     void testTextFileStreamReader() {
-        RandomAccessStream stream = TextFileStreamReader.inputFileOpen("src/test/resources/inputdata.txt");
-        assertEquals('i', TextFileStreamReader.inputFileNextChar(stream));
-        TextFileStreamReader.inputFileSeekBack(stream);
-        assertEquals('i', TextFileStreamReader.inputFileNextChar(stream));
-        assert TextFileStreamReader.inputFileHasLines(stream);
-        TextFileStreamReader.inputFileSkipLn(stream);
-        assertEquals("Integer", TextFileStreamReader.inputFileNextString(stream, 7));
-        assertEquals(42, TextFileStreamReader.inputFileNextInt(stream));
-        assertEquals(Integer.MIN_VALUE, TextFileStreamReader.inputFileNextInt(stream)); // -2147483648
-        assertEquals(Integer.MAX_VALUE, TextFileStreamReader.inputFileNextInt(stream)); // 2147483647
-        TextFileStreamReader.inputFileSkipLn(stream);
-        assertEquals(1, TextFileStreamReader.inputFileNextInt(stream)); // "  1"
-        assertEquals(2, TextFileStreamReader.inputFileNextInt(stream)); // "\t1"
-        TextFileStreamReader.inputFileSkipLn(stream);
-        assertEquals(0.25D, TextFileStreamReader.inputFileNextDouble(stream)); // 0.25
-        assertEquals(0.25D, TextFileStreamReader.inputFileNextDouble(stream)); // 0.25;
-        TextFileStreamReader.inputFileSkipLn(stream);
-        assertEquals(1.7976931348623157, TextFileStreamReader.inputFileNextDouble(stream)); // "\t1.7976931348623157"
-        assertEquals(-2.2250738585072014, TextFileStreamReader.inputFileNextDouble(stream)); // "\t-2.2250738585072014"
-        assert !TextFileStreamReader.inputFileHasLines(stream);
+        RandomAccessStream stream = inputFileOpen("src/test/resources/inputdata.txt");
+        assertEquals('i', inputFileNextChar(stream));
+        inputFileSeekBack(stream);
+        assertEquals('i', inputFileNextChar(stream));
+        assert inputFileHasLines(stream);
+        inputFileSkipLn(stream);
+        assertEquals("Line ", inputFileNextLine(stream));
+        assertEquals("Integer", inputFileNextString(stream, 7));
+        assertEquals(42, inputFileNextInt(stream));
+        assertEquals(Integer.MIN_VALUE, inputFileNextInt(stream)); // -2147483648
+        assertEquals(Integer.MAX_VALUE, inputFileNextInt(stream)); // 2147483647
+        inputFileSkipLn(stream);
+        assertEquals(1, inputFileNextInt(stream)); // "  1"
+        assertEquals(2, inputFileNextInt(stream)); // "\t1"
+        inputFileSkipLn(stream);
+        assertEquals(0.25D, inputFileNextDouble(stream)); // 0.25
+        assertEquals(0.25D, inputFileNextDouble(stream)); // 0.25;
+        inputFileSkipLn(stream);
+        assertEquals(1.7976931348623157, inputFileNextDouble(stream)); // "\t1.7976931348623157"
+        assertEquals(-2.2250738585072014, inputFileNextDouble(stream)); // "\t-2.2250738585072014"
+        assert !inputFileHasLines(stream);
     }
 
     @Test
-    void linesCount() {
-        int linesCount = TextFileStreamReader.linesCount("src/test/resources/inputdata.txt");
-        assertEquals(10, linesCount);
+    void testLinesCount() {
+        int linesCount = linesCount("src/test/resources/inputdata.txt");
+        assertEquals(9, linesCount);
     }
 }
